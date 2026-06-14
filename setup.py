a145bcd3012f859f3108407bc1bee035637c3cb0
GilmarCorreia <gilmarcorreiajeronimo@gmail.com>
2026-06-14 19:25:32 -0300

    fix: fixinging the linters with the colcon test

--- setup.py
+++ setup.py
@@ -1,32 +1,41 @@
-import os
 from glob import glob
+import os
 
 from setuptools import find_packages, setup
 
-package_name = "challenge_bringup"
+package_name = 'challenge_bringup'
 
 setup(
     name=package_name,
-    version="0.0.1",
-    packages=find_packages(exclude=["test"]),
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
-        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (
+          'share/ament_index/resource_index/packages',
+          ['resource/' + package_name]
+        ),
+        ('share/' + package_name, ['package.xml']),
+        (
+          os.path.join('share', package_name, 'launch'),
+          glob('launch/*.launch.py')
+        ),
+        (
+          os.path.join('share', package_name, 'config'),
+          glob('config/*.yaml')
+        ),
     ],
-    install_requires=["setuptools"],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="gilmar",
-    maintainer_email="gilmarcorreiajeronimo@gmail.com",
-    description="Challenge bringup package.",
-    license="BSD-2-Clause",
+    maintainer='gilmar',
+    maintainer_email='gilmarcorreiajeronimo@gmail.com',
+    description='Challenge bringup package.',
+    license='BSD-2-Clause',
     extras_require={
-        "test": [
-            "pytest",
+        'test': [
+            'pytest',
         ],
     },
     entry_points={
-        "console_scripts": [],
+        'console_scripts': [],
     },
 )
